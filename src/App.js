a4cae8eb2f33c537ba05e13bc86ef504ba6ef6c7
K-2052 <k@2052.me>
2016-02-01 10:00:31 -0800

    fix app class definition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./styles/App.css"
 
-export default React.createClass({
+class App extends React.Component {
   render() {
     return <div>
       <h2>Welcome to React</h2>
     </div>
   }
-})
+}
+
+export default App;
